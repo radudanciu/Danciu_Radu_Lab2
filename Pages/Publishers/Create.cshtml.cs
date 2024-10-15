33efb399db87ab443df1c47050c4e7a3046c0ac9
Radu Danciu <radudanciu1407@yahoo.com>
2024-10-15 23:56:00 +0300

    Add project files.

---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Danciu_Radu_Lab2.Data;
+using Danciu_Radu_Lab2.Models;
+
+namespace Danciu_Radu_Lab2.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Danciu_Radu_Lab2.Data.Danciu_Radu_Lab2Context _context;
+
+        public CreateModel(Danciu_Radu_Lab2.Data.Danciu_Radu_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
